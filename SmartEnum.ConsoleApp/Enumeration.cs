255b9b718910061d4e2bcd0d8c5d2a5259a2a709
Cuma KÖSE <turkmvc@gmail.com>
2024-03-28 00:13:35 +0300

    Add project files.

---
+++ SmartEnum.ConsoleApp/Enumeration.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+namespace SmartEnum.ConsoleApp;
+public abstract class Enumeration<TEnum>
+    where TEnum : Enumeration<TEnum>
+{
+    public static readonly Dictionary<string, TEnum> Enumerations = CreateEnumerations();
+
+    protected Enumeration(string value, string name)
+    {
+        Value = value;
+        Name = name;
+    }
+
+    public string Value { get; protected set; } = string.Empty;
+    public string Name { get; protected set; } = string.Empty;
+
+    public static TEnum? FromValue(string value)
+    {
+        return Enumerations.TryGetValue(value, out TEnum? enumeration) ? enumeration : null;
+    }
+
+    public static TEnum? FromName(string name)
+    {
+        return Enumerations.Values.SingleOrDefault(e => e.Name == name);
+    }
+
+    private static Dictionary<string, TEnum> CreateEnumerations()
+    {
+        var enumerationType = typeof(TEnum);
+
+        var fieldsForType = enumerationType
+            .GetFields(
+                BindingFlags.Public |
+                BindingFlags.Static |
+                BindingFlags.FlattenHierarchy
+            )
+            .Where(fieldInfo => enumerationType.IsAssignableFrom(fieldInfo.FieldType))
+            .Select(fieldInfo => (TEnum)fieldInfo.GetValue(default)!);
+
+            return fieldsForType.ToDictionary(x => x.Value);
+    }
+}
---
+++ SmartEnum.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using SmartEnum.ConsoleApp.Enums;
+
+
+CreditCard creditCardValue = CreditCard.Platinum;
+
+var discound = creditCardValue switch
+{
+    CreditCard.Standard => 0.01,
+    CreditCard.Platinum => 0.10,
+    CreditCard.Premium => 0.20,
+    CreditCard.Gold => 0.30,
+    _ => throw new NotImplementedException()
+};
+
+CreditCard2 creditCardValue2 = CreditCard2.FromValue(2);
+var discound2 = creditCardValue2.GetDiscountRate();
+
+Console.WriteLine($"Credit card type: {creditCardValue2} discound: {discound2}");
+
+
+var creditCard3 = CreditCard3.FromValue("Cuma 1");
+
+Console.WriteLine(creditCard3.Name);
